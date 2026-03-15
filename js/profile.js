94de7e7d45da823425a96bba8125d1c480e2952c
basma <basmatoumanar55@gmail.com>
2026-03-15 20:51:37 +0000

    add

--- js/profile.js
+++ js/profile.js
@@ -1,155 +1,224 @@
-const token = localStorage.getItem("jwt")
+const usernameLogin = document.getElementById('username-login')
+const password = document.getElementById('password')
+const loginform = document.getElementById('login-form')
+const loginPage = document.getElementById('login-page')
+const profilePage = document.getElementById('profile-page')
 
-fetch("https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql", {
-  method: "POST",
-  headers: {
-    "Authorization": "Bearer " + token,
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    query: `
-{
-  user {
-    login
-    firstName
-    lastName
-    avatarUrl
-    auditRatio
-    totalUp
-    totalDown
-
-    success: audits_aggregate(
-      where: { closureType: { _eq: succeeded } }
-    ) {
-      aggregate {
-        count
-      }
-    }
-
-    failed: audits_aggregate(
-      where: { closureType: { _eq: failed } }
-    ) {
-      aggregate {
-        count
-      }
-    }
-
-    events(where: {cohorts: {labelName: {_is_null: false}}}) {
-      cohorts {
-        labelName
-      }
-    }
+function Showpage(s) {
+  if (s === 'profile') {
+    loginPage.style.display = 'none'
+    profilePage.style.display = 'block'
+    ShowProfile()
   }
-
-  xp: transaction_aggregate(
-    where: {
-      type: {_eq: "xp"}
-      event: {object: {name: {_eq: "Module"}}}
-    }
-  ) {
-    aggregate {
-      sum {
-        amount
-      }
-    }
-  }
-
-  level: transaction_aggregate(
-    where: {
-      type: {_eq: "level"}
-      event: {object: {name: {_eq: "Module"}}}
-    }
-  ) {
-    aggregate {
-      max {
-        amount
-      }
-    }
-  }
-
-  transaction(
-    where: {
-      type: {_eq: "xp"}
-      event: {object: {name: {_eq: "Module"}}}
-      object: {type: {_eq: "project"}}
-    }
-    order_by: {createdAt: asc}
-  ) {
-    amount
-    createdAt
-    object {
-      name
-      type
-    }
+  if (s === 'login') {
+    loginPage.style.display = 'block'
+    profilePage.style.display = 'none'
   }
 }
-        `
-  })
+
+
+loginform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const user = usernameLogin.value;
+    const pass = password.value;
+
+    const token = btoa(user + ':' + pass);
+
+    fetch('https://learn.zone01oujda.ma/api/auth/signin', {
+        method: "POST",
+        headers: {
+            "Authorization": "Basic " + token,
+            "Content-Type": "application/json"
+        },
+        body: null
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Invalid credentials');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const errorMessage = document.getElementById("error-message");
+            if (data.jwt || typeof data === 'string') {
+                const jwt = data.jwt || data;
+                localStorage.setItem("jwt", jwt);
+
+                // redirection vers profile
+                Showpage('profile')
+              //  document.getElementById("error-message").textContent = 'Connexion réussie !'
+               // alert("Connexion réussie !")
+                // window.location.href = "profile.html"; // redirect après login
+            } else {
+                alert("Erreur lors de login");
+            }
+        })
+        .catch(err => {
+            alert("user name or password incorrect")
+            console.error(err);
+        })
 })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    // const amountXp = data.data.transaction
-    const firstName = data.data.user[0].firstName
-    const lastName = data.data.user[0].lastName
-    const avatar = data.data.user[0].avatarUrl
-    const cohort = data.data.user[0].events[0].cohorts[0].labelName
-    const totalXp = data.data.xp.aggregate.sum.amount
-    const level = data.data.level.aggregate.max.amount
-    let rank = ""
-    if (level < 10) {
-      rank = "Aspiring developer"
-    } else if (level > 10 && level < 20) {
-      rank = "Beginner developer"
-    } else if (level > 20 && level < 30) {
-      rank = "Apprentice developer"
-    } else if (level > 30 && level < 40) {
-      rank = "Assistant developer"
-    } else if (level > 40 && level < 50) {
-      rank = "Basic developer"
-    } else if (level > 50) {
-      rank = "Junior developer"
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+function ShowProfile() {
+  
+  const token = localStorage.getItem("jwt")
+  fetch("https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql", {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + token,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: `
+  {
+    user {
+      login
+      firstName
+      lastName
+      avatarUrl
+      auditRatio
+      totalUp
+      totalDown
+  
+      success: audits_aggregate(
+        where: { closureType: { _eq: succeeded } }
+      ) {
+        aggregate {
+          count
+        }
+      }
+  
+      failed: audits_aggregate(
+        where: { closureType: { _eq: failed } }
+      ) {
+        aggregate {
+          count
+        }
+      }
+  
+      events(where: {cohorts: {labelName: {_is_null: false}}}) {
+        cohorts {
+          labelName
+        }
+      }
     }
-    document.getElementById("current-rank").textContent = rank
-    document.getElementById("level").textContent = level
-    document.getElementById("total-xp").textContent = totalXp / 1000 + ' KB'
-    document.getElementById("avatar").src = avatar
-    document.getElementById("cohort").textContent = cohort
-    document.getElementById("username").textContent =
-      firstName + " " + lastName
-
-    const transactions = data.data.transaction.sort(
-      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
-    )
-    let slice = []
-    transactions.forEach(transaction => {
-      slice.push({
-        amount: transaction.amount,
-        project: transaction.object.name,
-        creatDate: transaction.createdAt
-      })
-    });
-    drawBarChart(slice)
-/*
-Audit Ratio
-Total Up
-Total Down
-Number of successful audits
-Number of failed audits
-*/
-    const AuditRatio = data.data.user[0].auditRatio
-    const TotalUp = data.data.user[0].totalUp
-    const TotalDown = data.data.user[0].totalDown
-    const successfulAudits = data.data.user[0].success.aggregate.count
-    const failedAudits = data.data.user[0].failed.aggregate.count
-
-    document.getElementById('Audit-Ratio').textContent = Math.round(AuditRatio * 10)/10 
-    document.getElementById('Total-Up').textContent =  Math.round((TotalUp / 1000000) * 100) / 100 + ' MB'
-    document.getElementById('Total-Down').textContent = Math.round((TotalDown / 1000000) * 100) / 100 + ' MB'
-    document.getElementById('successful-audits').textContent = successfulAudits
-    document.getElementById('failed-audits').textContent = failedAudits
+  
+    xp: transaction_aggregate(
+      where: {
+        type: {_eq: "xp"}
+        event: {object: {name: {_eq: "Module"}}}
+      }
+    ) {
+      aggregate {
+        sum {
+          amount
+        }
+      }
+    }
+  
+    level: transaction_aggregate(
+      where: {
+        type: {_eq: "level"}
+        event: {object: {name: {_eq: "Module"}}}
+      }
+    ) {
+      aggregate {
+        max {
+          amount
+        }
+      }
+    }
+  
+    transaction(
+      where: {
+        type: {_eq: "xp"}
+        event: {object: {name: {_eq: "Module"}}}
+        object: {type: {_eq: "project"}}
+      }
+      order_by: {createdAt: asc}
+    ) {
+      amount
+      createdAt
+      object {
+        name
+        type
+      }
+    }
+  }
+          `
+    })
   })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      // const amountXp = data.data.transaction
+      const firstName = data.data.user[0].firstName
+      const lastName = data.data.user[0].lastName
+      const avatar = data.data.user[0].avatarUrl
+      const cohort = data.data.user[0].events[0].cohorts[0].labelName
+      const totalXp = data.data.xp.aggregate.sum.amount
+      const level = data.data.level.aggregate.max.amount
+      let rank = ""
+      if (level < 10) {
+        rank = "Aspiring developer"
+      } else if (level > 10 && level < 20) {
+        rank = "Beginner developer"
+      } else if (level > 20 && level < 30) {
+        rank = "Apprentice developer"
+      } else if (level > 30 && level < 40) {
+        rank = "Assistant developer"
+      } else if (level > 40 && level < 50) {
+        rank = "Basic developer"
+      } else if (level > 50) {
+        rank = "Junior developer"
+      }
+      document.getElementById("current-rank").textContent = rank
+      document.getElementById("level").textContent = level
+      document.getElementById("total-xp").textContent = totalXp / 1000 + ' KB'
+      document.getElementById("avatar").src = avatar
+      document.getElementById("cohort").textContent = cohort
+      document.getElementById("username").textContent =
+        firstName + " " + lastName
+  
+      const transactions = data.data.transaction.sort(
+        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      )
+      let slice = []
+      transactions.forEach(transaction => {
+        slice.push({
+          amount: transaction.amount,
+          project: transaction.object.name,
+          creatDate: transaction.createdAt
+        })
+      });
+      drawBarChart(slice)
+  
+  /*
+  Audit Ratio
+  Total Up
+  Total Down
+  Number of successful audits
+  Number of failed audits
+  */
+      const AuditRatio = data.data.user[0].auditRatio
+      const TotalUp = data.data.user[0].totalUp
+      const TotalDown = data.data.user[0].totalDown
+      const successfulAudits = data.data.user[0].success.aggregate.count
+      const failedAudits = data.data.user[0].failed.aggregate.count
+  
+      document.getElementById('Audit-Ratio').textContent = Math.round(AuditRatio * 10)/10 
+      document.getElementById('Total-Up').textContent =  Math.round((TotalUp / 1000000) * 100) / 100 + ' MB'
+      document.getElementById('Total-Down').textContent = Math.round((TotalDown / 1000000) * 100) / 100 + ' MB'
+      document.getElementById('successful-audits').textContent = successfulAudits
+      document.getElementById('failed-audits').textContent = failedAudits
+    })
 
+}  
 
 function drawBarChart(slice) {
   const svg = document.getElementById("xp-graph")
